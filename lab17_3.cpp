6ab8bdbf979ae0c7c443dc7d7334903bef5ab7d4
Chiramert <fortjiramet10@gmail.com>
2025-02-07 22:49:58 +0700

    lab17_3.cpp

--- lab17_3.cpp
+++ lab17_3.cpp
@@ -1,39 +1,48 @@
-#include<iostream>
-#include<cstdlib>
-#include<ctime>
-#include<vector>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <vector>
 using namespace std;
 
-______________ randomVector(unsigned int N){	
-    vector<int> v;
-    for(unsigned int i = 0; i < N; i++) v.______________(rand()%10);
-    ______________;
+vector<int> randomVector(unsigned int N)
+{
+	vector<int> v;
+	for (unsigned int i = 0; i < N; i++)
+		v.push_back(rand() % 10);
+	return v;
 }
 
-______________ showVector(______________){
+void showVector(const vector<int> &v)
+{
 	cout << "[";
-	for(unsigned int i = 0; _____________________; i++){
-		cout << _________________;
-		if(_________________) cout << "]";
-		else cout << " ";
+	for (unsigned int i = 0; i < v.size(); i++)
+	{
+		cout << v[i];
+		if (i == v.size() - 1)
+			cout << "]";
+		else
+			cout << " ";
 	}
 }
 
-_________________ dotProduct(_____________________________){
+int dotProduct(const vector<int> &v1, const vector<int> &v2)
+{
 	int sum = 0;
-	for(unsigned int i = 0; _____________________; i++) sum += _________________;	
-	_________________;
+	for (unsigned int i = 0; i < v1.size(); i++)
+		sum += v1[i] * v2[i];
+	return sum;
 }
 
-int main(){
+int main()
+{
 	srand(time(0));
 	vector<int> x = randomVector(5);
 	vector<int> y = randomVector(5);
-	int z = dotProduct(x,y);
+	int z = dotProduct(x, y);
 	showVector(x);
 	cout << " . ";
 	showVector(y);
 	cout << " = " << z;
-	
+
 	return 0;
 }
